fc3c54b31fbcc6ed1def936ebd9890df28ccbfd7
Rajat Sharma <rajatsharma.9001@gmail.com>
2018-09-06 16:14:22 +0530

    Second Attempt

---
+++ MySecureKid/src/test/resources/Reporting/Reporter.java
@@ -0,0 +1,30 @@
+package Reporting;
+
+import java.io.File;
+import java.util.Date;
+
+import com.relevantcodes.extentreports.ExtentReports;
+
+public class Reporter {
+	
+	private static ExtentReports Extent;
+	
+	public static ExtentReports getInsatance() {
+		
+		if(Extent==null) {
+			Date d=new Date();
+			String FileName=d.toString().replace(":", "_").replace(" ", "_")+".html";
+			Extent=new ExtentReports("D:\\ExtentReports\\"+FileName,true);
+			
+			Extent.loadConfig(new File(System.getProperty("user.dir")+"\\ReportsConfig.xml"));
+			Extent.addSystemInfo("Selenium Version", "Selenium 3.12");
+			Extent.addSystemInfo("Environmrnt","Testing");
+		}
+		
+
+
+		
+return Extent;
+	}
+
+}

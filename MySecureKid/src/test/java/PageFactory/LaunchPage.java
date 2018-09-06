62553e82c8799daa99aa39684b1e9e9cbbdbe442
Rajat Sharma <rajatsharma.9001@gmail.com>
2018-09-06 18:01:37 +0530

    MultipleTestcases

--- MySecureKid/src/test/java/PageFactory/LaunchPage.java
+++ MySecureKid/src/test/java/PageFactory/LaunchPage.java
@@ -7,29 +7,29 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LaunchPage {
-	
-	 WebDriver driver;
-	
-@FindBy(xpath= "//*[@id=primary-menu]/li[4]/a")
+
+	WebDriver driver;
+
+	@FindBy(css="#primary-menu > li.menu-item-2514.aligned-left > a")
 	WebElement shop;
 
-@FindBy(xpath="//*[@id=content]/div[2]/div/div[3]/div/div[3]/div[2]/div/a[3]/img")
-WebElement Pink400T;
+	@FindBy(css="#content > div.products.products-grid > div > div.col-lg-6.col-md-6.col-sm-6.col-xs-6.grid.product.post-3588.product.type-product.status-publish.has-post-thumbnail.product_cat-smart-watch.product_tag-gps-watch.product_tag-kid-security-watch.product_tag-smart-watch.product_tag-touch-watch.first.instock.taxable.shipping-taxable.purchasable.product-type-variable > div > div.block-inner > div > div > a > span")
+	WebElement Black600W;
 
 
-public LaunchPage(WebDriver driver) {
-	this.driver=driver;
-	PageFactory.initElements(driver, this);
-}
+	public LaunchPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
 
-public void clickOnshop(WebDriver driver) {
-	shop.click();
+	public void clickOnshop(WebDriver driver) throws InterruptedException {
+
+		shop.click();
 
 	}
-public void clickOnColour(WebDriver driver,String colour) {
-	if(colour.equalsIgnoreCase("pink")) {
-		Pink400T.click();
+	public void clickOnColour(WebDriver driver,String colour) {
+		if(colour.equalsIgnoreCase("Black")) {
+			Black600W.click();		}
 	}
-}
 
 }
--- MySecureKid/src/test/java/testCases/LauchPageTestCase.java
+++ MySecureKid/src/test/java/testCases/LauchPageTestCase.java
@@ -1,13 +1,21 @@
 package testCases;
 
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.session.FirefoxFilter;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 import PageFactory.LaunchPage;
+import Reporting.Reporter;
 
 public class LauchPageTestCase {
 
@@ -15,33 +23,57 @@ public class LauchPageTestCase {
 	private String URL;
 	private String BrowserType = "Chrome";
 	private LaunchPage launchPage;
+	private ExtentReports rep=new Reporter().getInsatance();
+	private ExtentTest test;
 
 	@BeforeClass
 	public void BeforeClass() {
+
+		//test= new ExtentTest("MySecurekid", "description");
 		if (BrowserType.equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver", "D:\\New Webdrivers\\chromedriver.exe");
 			driver = new ChromeDriver();
-		} else if (BrowserType.equalsIgnoreCase("Mozilla")) {
-			System.setProperty("webdriver.gecko.driver", "D:\\New Webdrivers\\geckodriver.exe");
-			driver = new ChromeDriver();
 		}
-		if (driver != null) {
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
-		} else
+
+		else {
 			System.out.println("Driver is not initialized");
+		}
 		URL = "https://www.mysecurekid.com";
 
-		driver = new ChromeDriver();
 		launchPage = new LaunchPage(driver);
+		test =rep.startTest("MySecurekid");
+		test.log(LogStatus.INFO, "Launching "+BrowserType +"Browser");
 
 		driver.get(URL);
+		test.log(LogStatus.INFO,"Launching the website");
 	}
 
-	@Test
-	public void shop() {
-launchPage.clickOnshop(driver);		
-launchPage.clickOnColour(driver, "pink");
+	@Test(priority=1)
+	public void shop() throws InterruptedException {
+		launchPage.clickOnshop(driver);	
+		test.log(LogStatus.INFO,"Clicked on the Shop tab");
+		launchPage.clickOnColour(driver, "Black");
+		test.log(LogStatus.INFO,"Select Black Watch");
+
+		String ExpectedURL="https://mysecurekid.com/product/msk-600w/";
+		String ActualURL=driver.getCurrentUrl();
+		Assert.assertEquals(ActualURL, ExpectedURL) ;
+		test.log(LogStatus.PASS, "Test Case Passed");
+		rep.endTest(test);
+		
+	}
+	@Test(priority=2)
+	public void PageTitle() {
+		test=rep.startTest("Second Test Case");
+		test.log(LogStatus.INFO,"Starting Test Case 2");
+		test.log(LogStatus.PASS,"Demo Test");
+		
+	}
+	
+	@org.testng.annotations.AfterClass
+	public void AfterClass() {
+		rep.flush();
+		driver.close();
 	}
 
 }
